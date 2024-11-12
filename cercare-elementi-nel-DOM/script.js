8d05936edc1cd51d5c26636f05483a354454046b
Clarissa De Benedittis <debenedittisclarissa04@gmail.com>
2024-11-12 09:31:38 +0100

    esercizio cercare un elemento nel DOM

---
+++ cercare-elementi-nel-DOM/script.js
@@ -0,0 +1,21 @@
+//tutti gli elementi con classe "paragrafo" 
+const paragrafo = document.querySelectorAll(".paragrafo") 
+console.log(paragrafo);
+
+const paragrafoSpecifico = document.getElementsByClassName("paragrafo")
+console.log(paragrafoSpecifico);
+
+//l'elemento con id "elemento-3"
+const elementoId = document.querySelector("#elemento-3")
+console.log(elementoId);
+
+const elementoSpecifico = document.getElementById("elemento-3")
+console.log(elementoSpecifico);
+
+//tutti gli elementi h2 della pagina
+const h2 = document.querySelectorAll("h2")
+console.log(h2);
+
+
+
+
